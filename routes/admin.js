211f9f4c9bb0d80363e3b5e1e67417fa9504339e
Sujan Gautam <susangautam062@gmail.com>
2025-02-01 21:47:21 +0545

    changed adminroutes using the destructured functions

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { getAdminProfile, updateAdmin } = require('../controllers/adminControllers.js');
 const { authenticate, restrict } = require('../auth/verifyToken.js');
 
+// Define your routes using the destructured functions
 router.get('/profile/me', authenticate, restrict(["admin"]), getAdminProfile);
 router.put('/:id', authenticate, restrict(["admin"]), updateAdmin);
 
